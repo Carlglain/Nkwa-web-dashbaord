9ede4b6542b4200f9f72c959f659f33c4ee4f5d5
Carlstorm <carlglainnyuysemo@gmail.com>
2025-06-04 12:23:31 +0100

    final touches3

--- nkwa-dashboard/src/app/layout.js
+++ nkwa-dashboard/src/app/layout.js
@@ -20,7 +20,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
       </head>
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}
