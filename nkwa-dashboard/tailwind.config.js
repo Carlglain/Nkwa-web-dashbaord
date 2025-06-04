67ef7d8a558a8f3a3d53b3c89a43e31293a3b2c0
Carlstorm <carlglainnyuysemo@gmail.com>
2025-06-04 10:58:07 +0100

    corrected code for final deploy

--- nkwa-dashboard/tailwind.config.js
+++ nkwa-dashboard/tailwind.config.js
@@ -1,20 +1,16 @@
-/** @type {import('tailwindcss').Config} **/
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-inter)'],
+        inter: ['var(--font-inter)'],
         mono: ['var(--font-jetbrains-mono)'],
       },
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      }
     },
   },
   plugins: [],
